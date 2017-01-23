67e10d9b6244d8ceab2e94bacefaccb0f59260bd
isabellemao <isabelle2k1@gmail.com>
2017-01-22 19:38:25 -0500

    Did a lot
    If I think THIS is hard, I wonder how far I'll get in the real world :(
    13 lines of code in an hour!!!!!!!!!!!!

--- current_project.py
+++ current_project.py
@@ -1,4 +1,13 @@
 input_for_sorting = input("Paste in (or type, I won't judge) a list of things you'd like to be sorted!")
 split_input = input_for_sorting.split()
+
+for index in range(len(split_input)):
+  for index in range(len(split_input)):
+    if split_input[index] < split_input[index - 1] and split_input[index-1] != split_input[-1]: #if current one is earlier in the alphabet than the previous one
+      swap_down = split_input[index - 1]
+      swap_up = split_input[index]
+      split_input[index] = swap_down
+      split_input[index-1] = swap_up
+    
 for index in range(len(split_input)):
   print(split_input[index])
