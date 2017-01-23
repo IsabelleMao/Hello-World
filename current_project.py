a2503a3b9ab7cd48153634bd4914f0e1305fb12f
isabellemao <isabelle2k1@gmail.com>
2017-01-22 19:00:39 -0500

    Added a line
    Renamed input to have a better name...

--- current_project.py
+++ current_project.py
@@ -1,1 +1,2 @@
-input = input("Paste in (or type, I won't judge) a list of things you'd like to be sorted!")
+input_for_sorting = input("Paste in (or type, I won't judge) a list of things you'd like to be sorted!")
+split_input = input_for_sorting.split()
