ff5106b479f48a3b3ec1bb521cb1029690e67318
isabellemao <isabelle2k1@gmail.com>
2017-01-22 20:19:48 -0500

    Made it nearly twice as long
    ..thanks to comments. This is done for now, but I'll be elaborating on it tomorrow or maybe later.

--- current_project.py
+++ current_project.py
@@ -1,13 +1,23 @@
+#Ask the user for input
 input_for_sorting = input("Paste in (or type, I won't judge) a list of things you'd like to be sorted!")
 split_input = input_for_sorting.split()
+length_of_list = len(split_input)
 
-for index in range(len(split_input)):
+#The sorter
+#Sort all the words.
+for index in range(length_of_list):
+  #Needed for sorting just one word
   for index in range(len(split_input)):
-    if split_input[index] < split_input[index - 1] and split_input[index-1] != split_input[-1]: #if current one is earlier in the alphabet than the previous one
+    #If current one is earlier in the alphabet than the previous one:
+    if split_input[index] < split_input[index - 1] and split_input[index-1] != split_input[-1]:
+      #Swap their positions
       swap_down = split_input[index - 1]
       swap_up = split_input[index]
       split_input[index] = swap_down
       split_input[index-1] = swap_up
-    
-for index in range(len(split_input)):
-  print(split_input[index])
+
+#Print the new list, one by one.
+for index in range(length_of_list):
+  print(split_input[index] + " ")
+  
+#https://www.youtube.com/watch?v=WaNLJf8xzC4
