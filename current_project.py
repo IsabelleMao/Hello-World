a4c3aedb77da6b2ba519ad5fb62669762b5f67be
isabellemao <isabelle2k1@gmail.com>
2017-01-22 19:09:23 -0500

    A for loop?!?
    Prints out all elements of a list in separate lines.

--- current_project.py
+++ current_project.py
@@ -1,2 +1,4 @@
 input_for_sorting = input("Paste in (or type, I won't judge) a list of things you'd like to be sorted!")
 split_input = input_for_sorting.split()
+for index in range(len(split_input)):
+  print(split_input[index])
