82f05ebf43029e54c31afd8c89cb055a7acd8868
isabellemao <isabellemao@users.noreply.github.com>
2017-02-01 21:05:51 -0500

    Add file
    My default template for projects and assignments and whatnot.

---
+++ java/DefaultTemplate.java
@@ -0,0 +1,18 @@
+/*|
+|*|Project Name: 
+|*|Description: 
+|*|
+|*|Author: Isabelle Mao    
+|*|Date started: xx/oo/xoxo
+|*|Date finished: xx/oo/xoxo
+|*/
+
+import becker.robots.*;
+
+public class DefaultTemplate
+{
+   public static void main(String[] InsertArgumentHere)
+   {
+      //FILL IN THE BLANK!!
+   }
+}
