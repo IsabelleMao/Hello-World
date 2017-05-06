e54025fdde414779825a70765cf91eab19e91183
Isabelle <isabellemao@users.noreply.github.com>
2017-05-06 11:40:07 -0400

    Deleted a few unnecessary newlines
    !

--- python/stupid-calc.py
+++ python/stupid-calc.py
@@ -1,13 +1,21 @@
 #Style guide: New 'lines' shall be in the same print statement, or directly following an input.
 import math #Very funny, I know
 
-#make the input easier to read
-def ezyfy(raw):
-    dupe = raw
-    #All in lowercase, no extra spaces.
-    raw = dupe.lower().strip()
-    return raw
-
+#the new line function
+def nl():
+    print("------------------------------------------")
+    
+    
+#The function for adding
+def add_func():
+    print("Alright. Enter a list of numbers you want added, EACH SEPARATED BY A SPACE, and I'll return to you the sum!")
+    nl()
+    numbers = input()
+    numbers = split(numbers)
+    result = add(*numbers)
+    nl()
+    print("The sum of your numbers is {}".format(result))
+    
 #add all arguments together
 def add(*args):
     total_sum = 0
@@ -16,15 +24,67 @@ def add(*args):
         total_sum += args[number]
     return total_sum
 
+#The function for subtracting
+def subtract_func():
+    print("Certainly. Enter a single number to subtract from.")
+    nl()
+    original = float(input())
+    nl()
+    print("Now, enter a number/numbers you want subtracted from {}. Separate more than one number with a space".format(original))
+    nl()
+    subtract_this = input()
+    nl()
+    subtract_this = split(subtract_this)
+    result = subtract(original, *subtract_this)
+    print("Your result is {}".format(result))
+            
 #subtract an unknown amount of numbers from a given number
 def subtract(original, *args):
-  length = len(args)
-  for sub in range(length):
-    original -= args[sub]
-  return original
+    length = len(args)
+    for sub in range(length):
+        original -= args[sub]
+        return original
+
+#multiply all the numbers together
+def multiply_func():
+    print("Mhm. Enter all the numbers you want to multiply together, each separated by a space.")
+    nl()
+    numbers = input()
+    numbers = split(numbers)
+    result = multiply(*numbers)
+    nl()
+    print("The product of your numbers is {}".format(result))
   
+#multiply all the numbers together
+def multiply(*args):
+    product = 1
+    length = len(args)
+    for number in range(length):
+        product *= args[number]
+    return product
+
+#divide a number by an unknown amount of numbers
+def divide(original, *args):
+    length = len(args)
+    for sub in range(length):
+        original /= args[sub]
+    return original
+    
+#The function for dividing
+def divide_func():
+    print("Stupendous! Enter the dividend")
+    nl()
+    dividend = float(input())
+    nl()
+    print("Now, enter a number/numbers you want to divide {} by. Separate more than one number with a space".format(dividend))
+    nl()
+    divisors = input()
+    nl()
+    divisors = split(divisors)
+    quotient = divide(dividend, *divisors)
+    print("Your result is {}".format(quotient))
+    
 #solve for x in a quadratic formula 
-#NOT DONE IMPLEMENTING
 def quadratic_equation(a, b, c):
     discriminant = (b**2) - (4 * a * c)
     if discriminant < 0: #No solutions, 'cause they're imaginary
@@ -37,7 +97,29 @@ def quadratic_equation(a, b, c):
         sol2 = (-b - math.sqrt(discriminant))
         sol2 /= (2 * a)
         return [2, sol1, sol2]
-      
+#the function for the quadratic equation
+def quadratic_equation_func():
+    print("Okay! Please enter your a-value")
+    nl()
+    a = float(input())
+    nl()
+    print("Please enter your b-value")
+    nl()
+    b = float(input())
+    nl()
+    print("Almost there! c-value please")
+    nl()
+    c = float(input())
+    result = quadratic_equation(a, b, c)
+    if result[0] == 0:
+        nl()
+        print("With the values you gave me, there are no real solutions.")
+    elif result[0] == 1:
+        nl()
+        print("There is exactly one solution: {}".format(result[1]))
+    else:
+        nl()
+        print("There are TWO solutions! One is {} and the other is {}".format(result[1], result[2]))
     
 #split by space and return as a list of float numbers
 def split(arg):
@@ -59,10 +141,10 @@ def stupid_calc():
     -COMMONLY USED-
     -------------
     ADD
-    DIVIDE - WIP
     SUBTRACT
-    MULTIPLY- WIP
-    SUBTRACT
+    MULTIPLY
+    DIVIDE
+    EXPONENT - WIP
     -------------
     -FORMULAS-
     -------------
@@ -71,15 +153,14 @@ def stupid_calc():
     format: COMMAND- function
     Type the command to do the function. Note: not case sensitive :)
     """
-    print("""Enter HELP for a list of possible actions.
-------------------------------------------""")
+    print("Enter HELP for a list of possible actions.")
+    nl()
     while True:
-        print("""ready for a new command
-------------------------------------------""")
-        command = input()
-        command = ezyfy(command)
-        print("------------------------------------------")
-      
+        print("ready for a new command")
+        nl()
+        command = input().lower().strip()
+      #there would be a nl here
+        nl()
       #print help
         if command == "help":
             print(stupid_calc.__doc__)
@@ -87,49 +168,21 @@ def stupid_calc():
         elif command == "exit":
             print("Hope you had fun. Enjoy your day!")
             return
-          
       #add
         elif command == "add":
-            print("""Alright. Enter a list of numbers you want added, EACH SEPARATED BY A SPACE, and I'll return to you the sum!
-------------------------------------------""")
-            numbers = input()
-            numbers = split(numbers)
-            result = add(*numbers)
-            print("""------------------------------------------
-The sum of your numbers is {}""".format(result))
-
+            add_func()
       #subtract
         elif command == "subtract":
-            print("""Certainly. Enter a single number to subtract from.
-------------------------------------------""")
-            original = float(input())
-            print("------------------------------------------")
-            print("""Now, enter a number/numbers you want subtracted from {}. Separate more than one number with a space.
-------------------------------------------""".format(original))
-            subtract_this = input()
-            print("------------------------------------------")
-            subtract_this = split(subtract_this)
-            result = subtract(original, *subtract_this)
-            print("""Your result is {}
-------------------------------------------""".format(result))
-        elif command == "quadratic formula":
-            print("""Okay! Please enter your a-value
-------------------------------------------""")
-            a = int(input())
-            print("""------------------------------------------
-            Please enter your b-value
-------------------------------------------""")
-            b = int(input())
-            print("""------------------------------------------
-            Almost there! c-value please
-------------------------------------------""")
-            c = int(input())
-            result = quadratic_equation(a, b, c)
-            if result[0] == 0:
-                print("With the values you gave me, there are no real solutions.")
-            elif result[0] == 1:
-                print("There is exactly one solution: {}").format(result[2])
-            else:
-                print("There are TWO solutions! One is {} and the other is {}".format(result[1], result[2]))
+            subtract_func()
+      #multiply
+        elif command == "multiply":
+            multiply_func()
+       #divide
+        elif command == "divide":
+            divide_func()
+       #quadratic equation/formula
+        elif command == "quadratic formula" or command == "quadratic equation" or command == "qe":
+            quadratic_equation_func()
+        nl()
 
 stupid_calc()
