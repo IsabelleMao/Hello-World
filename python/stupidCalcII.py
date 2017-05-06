279c6e0195962ea3d1344b51f45821cc0710cef6
Isabelle <isabellemao@users.noreply.github.com>
2017-05-06 11:33:49 -0400

    Delete stupidCalcII.py

--- python/stupidCalcII.py
+++
@@ -1,5 +0,0 @@
-#because stupid calc is getting very stupid and I didn't document stuff at all :(
-import math #Very funny, I know
-def nl():
-    print("------------------------------------------")
-    
