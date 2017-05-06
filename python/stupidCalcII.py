c69ec33d75c23ec5f80f94e7e1094e25d5ad4b7e
Isabelle <isabellemao@users.noreply.github.com>
2017-05-06 11:30:14 -0400

    Update stupidCalcII.py

--- python/stupidCalcII.py
+++ python/stupidCalcII.py
@@ -1,2 +1,5 @@
 #because stupid calc is getting very stupid and I didn't document stuff at all :(
 import math #Very funny, I know
+def nl():
+    print("------------------------------------------")
+    
